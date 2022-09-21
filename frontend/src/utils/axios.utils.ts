3933dbaedcd6dee61dc4c94ee59e31c9f5db7d8d
blaketingyu <blaketantingyu@gmail.com>
2022-09-21 16:12:43 +0800

    fix: why is my backend not connecting

--- frontend/src/utils/axios.utils.ts
+++ frontend/src/utils/axios.utils.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosObj = axios.create({
-  baseURL: process.env.backend_url || "http://localhost:8080",
+  baseURL:
+    process.env.backend_url || "http://https://otot-b-bwo66nc7ba-as.a.run.app/",
   timeout: 1000,
 });
