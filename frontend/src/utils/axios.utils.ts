8f75773927be2e0477491e1c378d221e20376e73
blaketingyu <blaketantingyu@gmail.com>
2022-09-21 16:19:22 +0800

    fix: change prefix for env var

--- frontend/src/utils/axios.utils.ts
+++ frontend/src/utils/axios.utils.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosObj = axios.create({
-  baseURL:
-    process.env.backend_url || "http://https://otot-b-bwo66nc7ba-as.a.run.app/",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8080",
   timeout: 1000,
 });
