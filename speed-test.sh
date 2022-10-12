63a06cd98247605082b956628b799bc83637dace
blaketingyu <blaketantingyu@gmail.com>
2022-10-12 13:31:23 +0800

    fix: update shell script

--- speed-test.sh
+++ speed-test.sh
@@ -2,25 +2,25 @@
 
 #Check connection
 echo "Check connection\n"
-curl http://localhost:8080/redistest
+curl http://localhost:8080/redistest 
 
 echo "populate data\n"
 curl -d "" http://localhost:8080/redistest/populateDB > /dev/null 2>&1
 
-echo "Time taken for mongoDB to retrieve data\n" 
+echo "Recording time taken for mongoDB to retrieve data\n" 
 for (( i=0; i <= 10; ++i ))
 do 
-  start=`date +%s%N`;curl http://localhost:8080/redistest/mongotime > /dev/null 2>&1;end=`date +%s%N`;echo `expr $end - $start`
+  curl http://localhost:8080/redistest/mongotime > /dev/null 2>&1;
 done
 echo "\n"
 
-echo "Time taken for Redis to retrieve data" 
+echo "Recording Time taken for Redis to retrieve data" 
 for (( i=0; i <= 10; ++i ))
 do 
-  start=`date +%s%N`;curl http://localhost:8080/redistest/redistime > /dev/null 2>&1;end=`date +%s%N`;echo `expr $end - $start`
+  curl http://localhost:8080/redistest/redistime > /dev/null 2>&1;
 done
 echo "\n"
 
 echo "Teardown\n"
-curl -d "" http://localhost:8080/redistest/clearDB
-curl -d "" http://localhost:8080/redistest/clearcache
+curl -d "" http://localhost:8080/redistest/clearDB > /dev/null 2>&1;
+curl -d "" http://localhost:8080/redistest/clearcache > /dev/null 2>&1;
